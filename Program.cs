e34613c4fa96c72ff1df176ba4dfd9cee296b4c0
Goel <aayush.goel@innovasolutions.com>
2025-12-17 11:15:53 +0530

    changes in program.cs for deployment

--- Program.cs
+++ Program.cs
@@ -100,12 +100,8 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 app.Use(async (context, next) =>
 {
     if (context.Request.Path == "/")
